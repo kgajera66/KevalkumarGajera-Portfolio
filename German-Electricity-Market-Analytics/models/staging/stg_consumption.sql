03a2ebab26ad028c33bdd6d2c79ea4952bb25578
Kevalkumar Gajera <57314009+kgajera66@users.noreply.github.com>
2026-08-20 17:59:42 +0200

    Update stg_consumption.sql

--- German-Electricity-Market-Analytics/models/staging/stg_consumption.sql
+++ German-Electricity-Market-Analytics/models/staging/stg_consumption.sql
@@ -1,1 +1,22 @@
+-- models/staging/stg_consumption.sql
+-- Same 1:1 cleanup pattern as stg_generation -- just renaming and timezone conversion.
+
+with source as (
+
+    select * from {{ source('raw', 'raw_consumption') }}
+
+),
+
+renamed as (
+
+    select
+        timestamp_utc,
+        convert_timezone('UTC', 'Europe/Berlin', timestamp_utc) as timestamp_local,
+        netzlast_mw as total_load_mw
+
+    from source
+
+)
+
+select * from renamed
 
